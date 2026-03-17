c9b592dffb1b7ad6c22974713093370dd4edfd43
Your Name <you@example.com>
2026-03-17 14:55:39 +0300

    fix installpkg

--- LAB_6/scripts/install_pkgs.sh
+++ LAB_6/scripts/install_pkgs.sh
@@ -12,16 +12,17 @@ case "$ARCH" in
     aarch64) ARCH="arm64" ;;
 esac
 
-YQ="https://github.com/mikefarah/yq/releases/latest/download/yq_${KERNEL_NAME,,}_${ARCH}"
+YQ_BIN="yq_${KERNEL_NAME,,}_${ARCH}"
+YQ="https://github.com/mikefarah/yq/releases/latest/download/$YQ_BIN"
 KUSTOMIZE="https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/${KUSTOMIZE_VERSION}/kustomize_${KUSTOMIZE_VERSION}_${KERNEL_NAME,,}_${ARCH}.tar.gz"
 
-if [ ! -f "${HOME}/.local/bin/yq_linux_amd64" ]; then
+if [ ! -f "${HOME}/.local/bin/$YQ_BIN" ]; then
   mkdir -p ~/.local/bin
   echo "Downloading $YQ"
   curl -LO --progress-bar \
     "$YQ" \
-    --output-dir ~/.local/bin \
-    && chmod +x ~/.local/bin/yq_linux_amd64
+    --output-dir "$HOME/.local/bin" \
+    && chmod +x "$HOME/.local/bin/$YQ_BIN"
 fi
 
 if [ ! -f "${HOME}/.local/bin/kustomize" ]; then
@@ -29,10 +30,10 @@ if [ ! -f "${HOME}/.local/bin/kustomize" ]; then
   echo "Downloading $KUSTOMIZE"
   curl -sL --progress-bar \
     "$KUSTOMIZE" \
-    | tar xz -C ~/.local/bin kustomize \
-    && chmod +x ~/.local/bin/kustomize
+    | tar xz -C "$HOME/.local/bin" kustomize \
+    && chmod +x "$HOME/.local/bin/kustomize"
 fi
 
 echo "Kustomize: $(kustomize version)"
-yq_linux_amd64 --version
+$YQ_BIN --version
 
